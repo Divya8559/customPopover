bd5a7a4c40eefd96aca40952110b1cccebb5eb1d
Divya Nagaraju <divya@Divyas-MacBook-Pro.local>
2020-06-29 17:19:15 -0400

    style changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import { CategoryComponent } from './category/category.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     AppRoutingModule,
     MatMenuModule,
     BrowserAnimationsModule,
-    FlexLayoutModule 
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, ViewChild} from '@angular/core';
 import { Categories, Items } from '../category.model';
+import { MatMenuTrigger } from '@angular/material/menu';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-category',
@@ -9,6 +11,10 @@ import { Categories, Items } from '../category.model';
 export class CategoryComponent implements OnInit {
   @Input() option: Categories;
   @Input() items: any;
+  totalCount = 0;
+  showTotal: boolean;
+  cloneItems: any;
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
   constructor() { }
 
   ngOnInit(): void {
@@ -18,6 +24,44 @@ export class CategoryComponent implements OnInit {
     })
     this.items = selectedItem;
     console.log(this.items)
+    
   }
 
+  addValue(item) {
+    this.items.map((it) => {
+      if(it.itemName === item.itemName) {
+        it.itemCount = it.itemCount + 1;
+      }
+      return it;
+    });
+  }
+
+  subtractValue(item) {
+    this.items.map((it) => {
+      if(it.itemName === item.itemName) {
+        it.itemCount = it.itemCount - 1;
+      }
+      return it;
+    });
+  }
+  closeMenu(showResult) {
+    if(showResult ) {
+      this.totalCount = 0;
+      this.items.forEach(element => {
+        this.totalCount = this.totalCount + element.itemCount;
+      });
+    }
+    this.showTotal = showResult;
+    this.trigger.closeMenu();
+
+  }
+  checkState() {
+    if(this.showTotal === false) {
+      this.items = this.cloneItems;
+    }
+  }
+
+  getValue() {
+   this.cloneItems = _.cloneDeep(this.items);
+  }
 }
